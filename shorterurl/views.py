9a4f8afc0770a1408960a12482c5485eaef7255e
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-07-09 11:18:57 -0500

    validate fields

--- shorterurl/views.py
+++ shorterurl/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 import pdb
 from .models import Url
+import ast
 
 
 def index(request):
     try:
         if request.method == "POST":
-
             url = request.POST["url"]
-            try:
-                url_base62 = Url.decode_url_string_to_base62("", url)
-            except Exception as e:
-                context = {"error": e}
-            else:
+            url_base62 = Url.decode_url_string_to_base62("", url)
+            if not isinstance(url_base62, Exception):
                 object_url = Url.objects.create(url_text=url, url_base62=url_base62)
                 host_name_of_my_app = request.META["HTTP_HOST"]
 
@@ -20,6 +17,12 @@ def index(request):
                     "object_url": object_url,
                     "url_name": host_name_of_my_app + "/" + object_url.url_base62,
                 }
+
+                return render(request, "shorterurl/index.html", context)
+            else:
+                # pdb.set_trace()
+                error = ast.literal_eval(str(url_base62))[0]
+                context = {"error": error}
                 return render(request, "shorterurl/index.html", context)
 
     except Exception as e:
@@ -30,7 +33,9 @@ def index(request):
 
 
 def redirect_url(request, shorter_url):
-
-    get_url = Url.objects.filter(url_base62=shorter_url)[0]
-
-    return redirect(get_url.url_text)
+    try:
+        get_url = Url.objects.filter(url_base62=shorter_url)[0]
+    except Exception as e:
+        return render(request, "shorterurl/page_not_Found.html",)
+    else:
+        return redirect(get_url.url_text)
